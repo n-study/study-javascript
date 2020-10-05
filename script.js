f3a856396d6c53dd38047a18784eb9c783bb3449
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-05 23:00:54 +0900

    first day

---
+++ script.js
@@ -0,0 +1,82 @@
+// console.log("Hello, Javascript");
+
+let hello = "Hello, Javascript";
+let num = 1234;
+num = 4567;
+
+let num2 = num;
+num = 1234;
+// console.log(num, num2);
+
+let _num = 1234;
+let $$$$$$ = "Dollar";
+
+const a = "pithon";
+// a = "python";
+
+console.log(a);
+console.log("stop");
+
+const MAX_LENGHT = 20;
+
+const b = 1 + 1;
+
+console.clear()
+
+
+
+const str1 = "Hello. I'm ~~";
+const str2 = 'Hello. I\'m ~~';
+const str3 = `Hello. ${num}`;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+const num1 = 5134421343134;
+const num22 = 0x3a;
+const num3 = Infinity;
+const num4 = NaN; // Not a number;
+console.log(num1, num22, num3, num4);
+
+console.log(1 / 0);
+console.log(parseInt("aaaa"));
+
+const t = true;
+const f = false;
+
+const n = null; // 빈 거, 없는 거
+// const u = undefined; // 정의 되지 않은 거
+
+let undef;
+
+undef = 3;
+console.log(undef);
+undef = undefined;
+console.log(undef);
+
+console.clear();
+
+const obj = {
+  name: "이보성",
+  class: "6",
+  major: "HD",
+};
+
+console.log(obj);
+console.log(obj.name, obj.class, obj.major);
+
+console.clear();
+
+// console.log(typeof(str1));
+
+console.log(typeof str1);
+console.log(typeof num1);
+console.log(typeof t, typeof f);
+console.log(typeof n, typeof undef);
+console.log(typeof obj);
+
+const arr = ["a", "b", "c", "d"];
+console.log(arr);
+
+console.clear();
