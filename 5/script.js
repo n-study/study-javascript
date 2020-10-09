76d1ee44885377dcd8c32256a6f9e28715d96811
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-09 14:31:23 +0900

    special: pre

--- 5/script.js
+++ 5/script.js
@@ -1,1 +1,99 @@
 const log = console.log;
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const len = 3;
+
+// map
+
+
+
+
+
+
+
+
+
+
+
+
+
+// filter
+
+
+
+
+
+
+
+
+
+
+
+
+
+// reduce
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// take
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// takeWhile
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// go
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// pipe
