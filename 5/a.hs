e166e15d6d81dc7e03547027fe308b51343d140b
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-09 23:04:06 +0900

    haskell type hint

--- 5/a.hs
+++ 5/a.hs
@@ -1,3 +1,5 @@
+-- f :: Integral c => Int -> [c] -> c
 f n = sum . map (\x -> x ^ 2) . take n . filter odd
 
+-- f :: Integral c => Int -> [c] -> c
 f n = foldr (+) 0 . map (\x -> x ^ 2) . take n . filter odd
