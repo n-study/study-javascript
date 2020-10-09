281b2f7152466fe078cc141563babf271a7e8c08
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-09 23:04:11 +0900

    5: generator

--- 5/script.js
+++ 5/script.js
@@ -1,99 +1,110 @@
 const log = console.log;
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const len = 3;
+// generator
+function* inf() {
+  for (let i = 0;; i++) {
+    yield i;
+  }
+}
+const infGen = inf();
+// log(infGen.next().value);
+// log(infGen.next().value);
+// log(infGen.next().value);
+// log(infGen.next().value);
+// log(infGen.next().value);
+// log(infGen.next().value);
+// log(infGen.next().value);
+// log(infGen.next().value);
+// log(infGen.next().value);
+// log(infGen.next().value);
+
 
 // map
 
+const map = function* (f, xs) {
+  for (const x of xs) {
+    // log("map", x);
+    yield f(x);
+  }
+};
 
-
-
-
-
-
-
-
-
-
-
+const mapRecur = function* (f, xs) {
+  if (xs.length === 0) return;
+  yield f(xs[0]);
+  yield* mapRecur(f, xs.slice(1));
+};
 
 // filter
+const filter = function* (f, xs) {
+  for (const x of xs) {
+    if (f(x)) {
+      yield x;
+    }
+  }
+};
+const filterRecur = function* (f, xs) {
+  if (xs.length === 0) return;
+  if (f(xs[0])) yield xs[0];
+  yield* filterRecur(f, xs.slice(1));
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-// reduce
-
-
-
-
-
-
-
-
-
-
-
-
-
+const reduce = (f, acc, list) => {
+  for (const item of list) {
+    acc = f(acc, item);
+  }
+  return acc;
+};
 
 
 
 // take
-
-
-
-
-
-
-
-
-
-
-
-
+const take = function* (n, xs) {
+  let i = 0;
+  for (const x of xs) {
+    if (i++ === n) break;
+    yield x;
+  }
+};
 
 
 // takeWhile
+const takeWhile = function* (f, xs) {
+  for (const x of xs) {
+    if (!f(x)) break;
+    yield x;
+  }
+};
 
+// 리스트에서 홀수를 length만큼 뽑아서 제곱한 후 모두 더하기
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const len = 1;
 
-
-
-
-
-
-
-
-
-
-
-
+/* log(
+  reduce((a, b) => a + b, 0,
+    map(x => x ** 2,
+      take(len,
+        filter(x => x % 2, inf()))))) */
 
 
 // go
+const go = (x, ...fs) => reduce((a, f) => f(a), x, fs);
+
+go(
+  inf(),
+  xs => filter(x => x % 2, xs),
+  xs => take(len, xs),
+  xs => map(x => x ** 2, xs),
+  xs => reduce((a, b) => a + b, 0, xs),
+  log,
+);
+
+const length = (xs, n = 0) => {
+  if (xs.length === 0) return n;
+  return length(xs.slice(1), n + 1);
+};
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// pipe
+const test = function* () {
+  yield* [1, 2, 3, 4, 5, 6, 7, 8];
+};
