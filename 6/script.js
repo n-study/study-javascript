6d79294c3f7dff47cc12d0f67a7e54decc060176
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-13 22:58:27 +0900

    6: concurrent, promise

--- 6/script.js
+++ 6/script.js
@@ -2,14 +2,18 @@ function a() {
   for (let i = 0; i < 1000000; i++) {
     new Date().toString();
   }
+  console.log("end loop");
+  return "yes";
 }
 
 function b() {
   console.log("please call me asap!!!!!!");
 }
 
-a();
-b();
+// console.log("start");
+// a();
+// b();
+// console.log("end");
 
 
 
@@ -19,6 +23,10 @@ b();
 
 
 // setTimeout
+// console.log("start");
+// console.log(setTimeout(a, 0));
+// b();
+// console.log("end");
 
 
 
@@ -38,39 +46,42 @@ b();
 // setTimeout으로 반환값 받기??
 
 
-
-
-
-
-
-
+// console.log("start");
+// console.log(setTimeout(() => {
+//   const tmp = a();
+//   b();
+//   console.log(tmp);
+// }, 0));
+// b();
+// console.log("end");
 
 
 
 // 아... 그럼 값을 어떻게 받지...
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // 콜백
 
 
+function a2(cb) {
+  setTimeout(() => {
+    for (let i = 0; i < 1000000; i++) {
+      new Date().toString();
+    }
+    console.log("end loop");
+    cb("yes");
+  }, 0);
+  // return "yes";
+}
 
 
-
-
+// a2(x => {
+//   a2(y => {
+//     console.log(y);
+//   })
+//   console.log(x);
+// });
+// a2(console.log)
+// b();
 
 
 
@@ -78,25 +89,68 @@ b();
 
 // Promise
 
-
-
-
-
-
-
-
-
+// Promise.resolve(111);
 
 // 세가지 상태
 
-
-
-
-
-
-
-
-
-
-
 // Promise에서 값을 받아오기
+
+
+// const prom1 = Promise.resolve("yes");
+
+// console.log(prom1);
+
+// prom1.then(x => {
+//   console.log(x);
+// }).catch(x => {
+//   console.log("failed T.T");
+// });
+
+
+// const prom2 = Promise.reject("sorry")
+
+// console.log(prom2);
+
+// prom2.then(x => {
+//   console.log("success!");
+// }).catch(x => {
+//   console.log(x);
+// });
+
+
+
+
+// const prom3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     for (let i = 0; i < 1000000; i++) {
+//       new Date().toString();
+//     }
+//     console.log("end loop");
+//     resolve("yes");
+//   }, 0);
+// });
+
+// prom3.then(x => {
+//   console.log(`success: ${x}`);
+// });
+
+// console.log(`success: `, prom3);
+
+
+
+const prom4 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve("yes");
+    reject("sorry");
+  }, 3000);
+});
+
+prom4.then(x => {
+  console.log(`success: ${x}`);
+}).catch(err => {
+  console.log(`rejected: ${err}`);
+});
+
+console.log(prom4);
+
+
