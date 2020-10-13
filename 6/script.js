a6b9157b881aac54eb83958b1c6db2f496efc779
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-13 21:58:55 +0900

    6: pre

---
+++ 6/script.js
@@ -0,0 +1,102 @@
+function a() {
+  for (let i = 0; i < 1000000; i++) {
+    new Date().toString();
+  }
+}
+
+function b() {
+  console.log("please call me asap!!!!!!");
+}
+
+a();
+b();
+
+
+
+
+
+
+
+
+// setTimeout
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// setTimeout으로 반환값 받기??
+
+
+
+
+
+
+
+
+
+
+
+// 아... 그럼 값을 어떻게 받지...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 콜백
+
+
+
+
+
+
+
+
+
+
+
+// Promise
+
+
+
+
+
+
+
+
+
+
+// 세가지 상태
+
+
+
+
+
+
+
+
+
+
+
+// Promise에서 값을 받아오기
