7ae7cf675941d9493e9d94b297b422b89d4850ea
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-14 10:00:28 +0900

    7: make promise

--- 7/loadScript.js
+++ 7/loadScript.js
@@ -1,29 +1,10 @@
-function loadScript(src, callback, error) {
-  const script = document.createElement("script");
-  script.src = src;
-  document.body.append(script);
+function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+    script.src = src;
+    document.body.append(script);
 
-  script.onload = () => callback(script);
-  script.onerror = (err) => error("can't load script", err);
-}
-
-// loadScript는 비동기로 작동
-loadScript("./makeFunction.js", (makeScript) => {
-  console.log(`${makeScript.src} is loaded!`);
-
-  loadScript("./option.js", (optionScript) => {
-    console.log(`${optionScript.src} is loaded!`);
-
-    loadScript("./arguments.js", (argumentScript) => {
-      console.log(`${argumentScript.src} is loaded!`);
-
-      make(option, ...arguments);
-    }, (error, msg) => {
-      console.error(error, msg);
-    });
-  }, (error, msg) => {
-    console.error(error, msg);
+    script.onload = () => resolve(script);
+    script.onerror = (err) => reject("can't load script", err);
   });
-}, (error, msg) => {
-  console.error(error, msg);
-});
+}
