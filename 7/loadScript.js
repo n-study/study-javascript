7a2332b7358849e550d59fde6294ddc8ef531e66
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-14 10:07:25 +0900

    7: then then then then~~

--- 7/loadScript.js
+++ 7/loadScript.js
@@ -8,3 +8,17 @@ function loadScript(src) {
     script.onerror = (err) => reject("can't load script", err);
   });
 }
+
+loadScript("./makeFunction.js")
+  .then((script) => {
+    console.log(`${script.src} is loaded!`);
+    loadScript("./option.js");
+  })
+  .then((script) => {
+    console.log(`${script.src} is loaded!`);
+    loadScript("./argument.js");
+  })
+  .then((script) => {
+    console.log(`${script.src} is loaded!`);
+    make(option, ...arguments);
+  });
