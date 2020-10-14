9c5579cf78e7c0841417b2e0d364b6b9ba79d70f
CraftyDragon678 <craftydragon678@gmail.com>
2020-10-14 09:51:11 +0900

    7: callback hell...

--- 7/loadScript.js
+++ 7/loadScript.js
@@ -8,13 +8,19 @@ function loadScript(src, callback, error) {
 }
 
 // loadScript는 비동기로 작동
-loadScript("./makeFunction.js", (script) => {
-  console.log(`${script.src} is loaded!`);
+loadScript("./makeFunction.js", (makeScript) => {
+  console.log(`${makeScript.src} is loaded!`);
 
-  loadScript("./option.js", (script) => {
-    console.log(`${script.src} is loaded!`);
+  loadScript("./option.js", (optionScript) => {
+    console.log(`${optionScript.src} is loaded!`);
 
-    make(option);
+    loadScript("./arguments.js", (argumentScript) => {
+      console.log(`${argumentScript.src} is loaded!`);
+
+      make(option, ...arguments);
+    }, (error, msg) => {
+      console.error(error, msg);
+    });
   }, (error, msg) => {
     console.error(error, msg);
   });
